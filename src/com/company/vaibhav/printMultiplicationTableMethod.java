07ab8f316c0ee3bb36d7c38de5012ef850422fd4
Vaibhav Anand <vaibhavaeshu@gmail.com>
2021-01-08 17:25:56 +0530

    Update 1

--- src/com/company/vaibhav/printMultiplicationTableMethod.java
+++ src/com/company/vaibhav/printMultiplicationTableMethod.java
@@ -1,6 +1,25 @@
 package com.company.vaibhav;
 
+import java.util.Scanner;
+
 public class printMultiplicationTableMethod
 {
-    
+    static void printMultiplication(int n)
+    {
+
+        System.out.println("Multiplication Table of " + n);
+        for (int i = 1; i<=10; i++)
+        {
+            System.out.println(n + " x " + i + " = " + n*i);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the number whose Multiplication Table is to be printed: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printMultiplication(n);
+    }
 }
